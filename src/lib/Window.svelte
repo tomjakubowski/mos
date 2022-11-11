bf23287eb0d434a4b0de6bbb454094d8cb240188
Tom Jakubowski <tom@crystae.net>
2022-11-11 11:57:20 -0800

    meowza

---
+++ src/lib/Window.svelte
@@ -0,0 +1,31 @@
+<script>
+	import '98.css/style.css';
+
+	export let title = '';
+
+	function handleClose() {
+		window.alert('close me please');
+	}
+</script>
+
+<div class="window">
+	<div class="title-bar">
+		<div class="title-bar-text">{title}</div>
+		<div class="title-bar-controls">
+			<button on:click={handleClose} aria-label="Close" />
+		</div>
+	</div>
+	<div class="window-body">
+		<img src="http://placekitten.com/300/300" alt="good kitty" />
+	</div>
+</div>
+
+<style>
+	.window {
+		width: var(--width, 300px);
+	}
+
+	.window-body {
+		overflow: scroll;
+	}
+</style>
